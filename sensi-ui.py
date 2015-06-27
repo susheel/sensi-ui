9af056934625c96d3a522fae6f60ec25baa11726
Susheel Varma <susheel.varma@gmail.com>
2015-06-27 21:59:56 +0100

    Updated Sensi with command string

--- sensi-ui.py
+++ sensi-ui.py
@@ -1,13 +1,15 @@
 import os
 from flask import Flask, render_template, request
 import paramiko
+from subprocess import Popen, PIPE
+from subprocess import call
 
 from settings import credentials
 
 SENSI_HOST = os.getenv('SENSI_HOST', "0.0.0.0")
 SENSI_PORT = os.getenv('SENSI_PORT', 8080)
 
-CMD_STR = "{script} {samples} "
+CMD_STR = "echo {parameters} > {workfolder}/{model}_parameters.txt && {script} {model} {workfolder}/{model}_parameters.txt {samples} {inputs} {outputs}"
 
 app = Flask(__name__)
 
@@ -36,14 +38,15 @@ def submit():
     'inputs' : request.form['inputs'],
     'outputs' : request.form['outputs'],
     'location' : request.form['location'],
-    'script' : request.form['script']
+    'script' : request.form['script'],
+    'workfolder' : request.form['workfolder']
   }
   error, logs = submit_ssh_command(params)
   print error, logs
   if error:
-    return render_template('error.html', logs=''.join(logs))
+    return render_template('error.html', logs=logs, params=params)
   else:
-    return render_template('success.html', logs=''.join(logs))
+    return render_template('success.html', logs=logs, params=params)
 
 # def submit_ssh_command(params):
 #   error = False
@@ -86,11 +89,20 @@ def submit():
 
 #   return error, logs
 
+def generate_cmd_string(params):
+  return CMD_STR.format(script=params['script'],
+      model=params['model'],
+      samples=params['samples'],
+      parameters=params['parameters'],
+      inputs=params['inputs'],
+      outputs=params['outputs'],
+      workfolder=params['workfolder'])
+
 def submit_ssh_command(params):
 
   error = False
   logs = []
-  logs.append("Trying to connect to %s" % params['location'])
+  logs.append("Trying to connect to %s..." % params['location'])
   try:
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -100,24 +112,37 @@ def submit_ssh_command(params):
     passwd = credentials[params['location']]['password']
 
     ssh.connect(host, username=user, password=passwd, timeout=5.0)
-    logs.append("Connected to %s" % params['location'])
+    logs.append("Connected to %s..." % params['location'])
+
+    logs.append("Submitting Sensi Job to %s..." % params['location'])
+    command = generate_cmd_string(params)
+    stdin, stdout, stderr = ssh.exec_command(command)
+    logs.append('Result of command %s is:' % command)
+
+    while not stdout.channel.exit_status_ready():
+       if stdout.channel.recv_ready():
+          logs.extend(stdout.readlines())
+
   except paramiko.AuthenticationException:
     error = True
-    logs.append("Authentication failed when connecting to %s" % params['location'])
+    logs.append("Authentication failed when connecting to %s." % params['location'])
   except:
-    logs.append("Could not SSH to %s" % params['location'])
-
-  logs.append("Submitting Sensi Job to %s" % params['location'])
-  stdin, stdout, stderr = ssh.exec_command('ls')
-
-  while not stdout.channel.exit_status_ready():
-    if stdout.channel.recv_ready():
-      logs.extend(stdout.readlines())
+    error = True
+    logs.append("Could not SSH to %s." % params['location'])
 
   ssh.close()
 
   return error, logs
 
+# wait for VPN to be properly up ...
+while True:
+    p = Popen(["/webapp/sensi-ui/configvpn"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate()
+    p = Popen(["route", "-n"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate()
+    if output.count("ppp0")==2 and output.count("eth0")>=2:
+        break
+    time.sleep(5)
 
 
 if __name__ == "__main__":
